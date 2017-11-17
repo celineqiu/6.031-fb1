426811ab276695ad4c40e4c3ea99428b1b57daf8
Rhea Lin <rhealin@mit.edu>
2017-11-17 16:58:00 -0500

    Updated tests for gadgets

--- test/flingball/GadgetTest.java
+++ test/flingball/GadgetTest.java
@@ -1,22 +1,89 @@
 package flingball;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import physics.Vect;
 
 
 public class GadgetTest {
     // Testing strategy for Gadget 
-    // TODO document testing strategy
-   
+    //   Test of variant of gadget
+    //     Square Bumper
+    //     Circle Bumper
+    //     Triangle Bumper
+    //     Absorber
+    //     Wall
+    //   Test all general methods of gadget
+    //     name
+    //     timeUntilCollison
+    //     velocityAfterCollision
+    //     trigger
+    //     action
+    //     drawIcon
+    //     copy
+    //     toString
+    //     equals
+    //     hashCode
+    //     addActionObject
+    //   Test specific methods of gadgets
+    
+    // covers Absorber
+    //     all general methods of gadget
+    //     specific methods of Absorber
+    //       getOrigin
+    //       getWidth
+    //       getHeight
+    //       getEdges
+    //       getCorners
+    //       getBalls
     @Test
-    public void testToStringAbsorber() {
-        String expected = "name: absorber" + "\n" +
+    public void testAbsorber() {
+        Absorber abs = new Absorber("absorber", 1, 1, 5, 5);
+        assertEquals("expected correct name", "absorber", abs.name());
+        // timeUntilCollision
+        // velocityAfterCollision
+        // trigger
+        // action
+        // drawIcon
+        // copy
+        Absorber absCopy = abs.copy();
+        assertEquals("expected same origin", abs.getOrigin(), absCopy.getOrigin());
+        assertEquals("expected same width", abs.getWidth(), absCopy.getWidth());
+        assertEquals("expected same height", abs.getHeight(), absCopy.getHeight());
+        assertEquals("expected same edges", abs.getEdges(), absCopy.getEdges());
+        assertEquals("expected same corners", abs.getCorners(), absCopy.getCorners());
+        assertEquals("expected same balls", abs.getBalls(), absCopy.getBalls());
+        
+        // toString
+        String expectedString = "name: absorber" + "\n" +
                           "origin: (1,1)" + "\n" +
                           "width: 5" + "\n" +
                           "height: 5";
-        Absorber abs = new Absorber("absorber", 1, 1, 5, 5);
-        assertEquals("expected same string", expected, abs.toString());
+        assertEquals("expected same string", expectedString, abs.toString());
+        
+        // equals
+        Absorber absSame = new Absorber("absorber", 1, 1, 5, 5);
+        assertEquals("expected same absorbers to be equal", absSame, abs);
+        assertEquals("expected copies to be equal", absCopy, abs);
+        assertEquals("expected symmetric equality", abs, abs);
+        
+        // hashCode
+        int expectedHashCode = abs.name().hashCode() + (int)abs.getOrigin().x() + (int)abs.getOrigin().y() + abs.getWidth() + abs.getHeight();
+        assertEquals("expected correct hashCode", expectedHashCode, abs.hashCode());
+        
+        // addActionObject
+        abs.addActionObject(absCopy);
+//        assertTrue("expected added action object to be in actionObjects", abs.getActionObjects().contains(absCopy));
+        
+        // getOrigin
+        assertEquals("expected correct origin", new Vect(1, 1), abs.getOrigin());
+        
+        // getWidth
+        // getEdges
+        // getCorners
+        // getBalls
     }
     
     @Test
