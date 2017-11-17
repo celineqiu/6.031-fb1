ca0b75c56a0ee6e6689f8285476ac5bbaa5928d6
Celine Qiu <cqiu@mit.edu>
2017-11-17 18:41:03 -0500

    fixed test compilation error

--- test/flingball/GUITest.java
+++ test/flingball/GUITest.java
@@ -1,27 +1,44 @@
-// Testing strategy for GUI
-// 
-// Use visual tests to evaluate the correctness of the GUI/main program
-//
-// Flingball
-//      no file specified -> default file
-//      valid flingball file path -> specified file
-//      valid flingball file path -> default file
-//
-// Game / Simulator
-//      balls: 
-//          0, 1, >1
-//          initial velocity 0, >0
-//      gadgets: 
-//          Absorber, CircleBumper, SquareBumper, TriangleBumper
-//          different Absorber sizes
-//          different TriangleBumper orientations - 0, 90, 180, 270
-//          Absorber triggered by another gadget
-//          self-triggering Absorber
-//          combinations of the different gadgets 
-//      things to check for:
-//          ball moves to bottom right corner of absorber
-//          ball shoots straight up from absorber
-//          ball collides with gadgets
-//          gadgets change color with hit/triggered (excluding Absorber and Wall)
-//          balls ejected with correct trigger
-//          balls reflect off gadgets (except Absorber)
+
+package flingball;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GUITest {
+    // Testing strategy for GUI
+    // 
+    // Use visual tests to evaluate the correctness of the GUI/main program
+    //
+    // Flingball
+    //       no file specified -> default file
+    //       valid flingball file path -> specified file
+    //       valid flingball file path -> default file
+    //
+    // Game / Simulator
+    //       balls: 
+    //           0, 1, >1
+    //           initial velocity 0, >0
+    //       gadgets: 
+    //           Absorber, CircleBumper, SquareBumper, TriangleBumper
+    //           different Absorber sizes
+    //           different TriangleBumper orientations - 0, 90, 180, 270
+    //           Absorber triggered by another gadget
+    //           self-triggering Absorber
+    //           combinations of the different gadgets 
+    //       things to check for:
+    //           ball moves to bottom right corner of absorber
+    //           ball shoots straight up from absorber
+    //           ball collides with gadgets
+    //           gadgets change color with hit/triggered (excluding Absorber and Wall)
+    //           balls ejected with correct trigger
+    //           balls reflect off gadgets (except Absorber)
+
+
+    @Test 
+    public void testGUI() {
+        assertTrue(true);
+    }
+
+}
